bf523afbd7fb90d1313a2590363c3f0393b91342
thalitsms <thalitsms.tm@gmail.com>
2021-05-10 21:48:34 -0300

    correções modal

--- front-end/src/app/tema/tema.component.ts
+++ front-end/src/app/tema/tema.component.ts
@@ -14,15 +14,15 @@ export class TemaComponent implements OnInit {
   tema: Tema = new Tema()
   listaTemas: Tema[]
 
-  idTema: number
-
   constructor(
     private router: Router,
     private temaService: TemaService,
 
     private route: ActivatedRoute
 
-    ) { }
+    ) {
+
+    }
 
   ngOnInit() {
     if(environment.token == ''){
@@ -30,52 +30,40 @@ export class TemaComponent implements OnInit {
       this.router.navigate(['/login'])
     }
 
-      this.findAllTemas()
-
-      this.idTema = this.route.snapshot.params['id']
-      this.findByIdTema(this.idTema)
-
-      let id = this.route.snapshot.params["id"]
-      this.findByIdTema(id)
-
+    this.findAllTemas()
   }
 
   findAllTemas(){
-
     this.temaService.getAllTema().subscribe((resp: Tema[]) => {
       this.listaTemas = resp
-
     })
 
   }
 
-  findByIdTema(id: number){
-    this.temaService.getByIdTema(id).subscribe((resp: Tema) =>{
-      this.tema = resp
-    })
-  }
+  // findByIdTema(id: number){
+  //   this.temaService.getByIdTema(id).subscribe((resp: Tema) => {
+  //     this.tema = resp
+  //   })
+  // }
 
   cadastrar(){
     this.temaService.postTema(this.tema).subscribe((resp: Tema) => {
-      this.tema = resp
       alert('Tema cadastrado com sucesso!')
       this.findAllTemas()
-      this.tema = new Tema()
     })
   }
 
   apagar(){
-    this.temaService.deleteTema(this.idTema).subscribe(() => {
+    this.temaService.deleteTema(this.tema.id).subscribe(() => {
       alert("Tema apagado com sucesso!")
-      this.router.navigate(["/tema"])
+      this.findAllTemas()
     })
   }
 
   atualizar(){
     this.temaService.putTema(this.tema).subscribe((resp: Tema) =>{
-      this.tema = resp
       alert("Tema atualizado com sucesso!")
-      this.router.navigate(["/tema"])
+      this.findAllTemas()
     })
   }
 
